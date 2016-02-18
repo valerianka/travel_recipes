8ceacc64d72b5d34af8c9521381df97a402e6041
Valerie Gorbik <valerianka@me.com>
2016-02-17 21:40:16 -0800

    Add actual list of the recipes to the Recipe component

--- travel-recipes/client/recipeViews/recipeList.jsx
+++ travel-recipes/client/recipeViews/recipeList.jsx
@@ -1,4 +1,19 @@
 MainPage=React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      recipes: Recipes.find({}).fetch()
+    }
+  },
+
+  renderRecipes(){
+    return this.data.recipes.map((recipe) => {
+      return <Recipe key={recipe._id} recipe={recipe} />;
+    })
+
+  },
+
   render() {
     return <div>
       <div
@@ -6,6 +21,24 @@ MainPage=React.createClass({
       style={{backgroundImage: "url('/traveling/womanphotographingstupa.jpg')"}}>
       Discover the worlds most delicious vacations
       </div> 
+      {this.renderRecipes()}
     </div>;
   }
-})
+});
+
+Recipe=React.createClass({
+  render(){
+    console.log(this.props.recipe);
+    return <div>
+  <a href="/recipes/{this.props.recipe._id}">
+    <div className="thumb" style={{backgroundImage: `url(${this.props.recipe.mainImage})`}}>
+      <p className="name">{this.props.recipe.name} </p>
+      <p>{this.props.recipe.duration}</p>
+      <p>{this.props.recipe.location}</p>
+      <p className="username">Created by:</p>
+      <p>{this.props.recipe.userName} {this.props.recipe.dateCreatedFromNow}</p>
+    </div>
+  </a>
+</div>;
+  }
+});
