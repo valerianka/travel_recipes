28c623a13b285d993334b1ea6e81a527cecfc2c2
dierat <diedra.rater@gmail.com>
2016-02-25 10:23:14 -0800

    Make thumbnail border thinner

--- travel-recipes/client/recipeViews/recipeList.jsx
+++ travel-recipes/client/recipeViews/recipeList.jsx
@@ -33,18 +33,29 @@ MainPage=React.createClass({
 
 
 Recipe=React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      windowWidth: rwindow.innerWidth()
+    }
+  },
+
   render(){
     const recipeLink = `/recipes/${this.props.recipe._id}`;
+    const height = calculateHeight(this.data.windowWidth);
+    const backgroundImage = `url(${this.props.recipe.mainImage})`;
     return (
       <a href={recipeLink}>
-        <div className="thumb-wrapper">
-          <div className="thumb" style={{backgroundImage: `url(${this.props.recipe.mainImage})`}}>
-            <p className="name">{this.props.recipe.name} </p>
-            <p>{this.props.recipe.duration}</p>
-            <p>{this.props.recipe.location}</p>
-            <p className="username">Created by:</p>
-            <p>{this.props.recipe.userName} {this.props.recipe.dateCreatedFromNow}</p>
-          </div>
+        <div
+          className="thumb-wrapper"
+          style={{height: height, backgroundImage: backgroundImage}}
+        >
+          <p className="name">{this.props.recipe.name} </p>
+          <p>{this.props.recipe.duration}</p>
+          <p>{this.props.recipe.location}</p>
+          <p className="username">Created by:</p>
+          <p>{this.props.recipe.userName} {this.props.recipe.dateCreatedFromNow}</p>
         </div>
       </a>
     );
