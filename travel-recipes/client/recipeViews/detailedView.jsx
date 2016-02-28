3cab9f51384cec14603bb3efe4328034eeee9745
dierat <diedra.rater@gmail.com>
2016-02-28 13:14:02 -0800

    Rename Ingredients to IngregientsList, uncomment remaining react code

--- travel-recipes/client/recipeViews/detailedView.jsx
+++ travel-recipes/client/recipeViews/detailedView.jsx
@@ -57,57 +57,64 @@ DetailedView=React.createClass({
   },
 
   render(){
-    console.log("recipe = ", this.data.recipe);
-
     if (!this.data.recipe){
-      return (<div>
-        <Banner backgroundImage={'/traveling/womanlost.jpg'} bannerText={'Recipe not found'} />
+      return (
+        <div>
+          <Banner backgroundImage={'/traveling/womanlost.jpg'} bannerText={'Recipe not found'} />
 
-        <div className="container">
-          <p>I'm sorry but that recipe doesn't exist. Maybe it was deleted?</p>
-          <p>Head back to the <a href="/">homepage</a> to find a better one!</p>
+          <div className="container">
+            <p>I'm sorry but that recipe doesn't exist. Maybe it was deleted?</p>
+            <p>Head back to the <a href="/">homepage</a> to find a better one!</p>
+          </div>
         </div>
-      </div>);
+      );
     } else {
-      return null;
-        // <div>.
-          // <Banner backgroundImage={this.data.recipe.mainImage} bannerText={this.data.recipe.name}/>
-          //  <div className="container">
-          //    <div id="recipe-info">
-          //      <h3>Visit {this.data.recipe.location} for {this.data.recipe.duration}</h3>
-          //       <p>{this.data.recipe.summary}</p>
-          //     </div>
+      return (
+        <div>
+          <Banner backgroundImage={this.data.recipe.mainImage} bannerText={this.data.recipe.name}/>
+          <div className="container">
+            <div id="recipe-info">
+              <h3>Visit {this.data.recipe.location} for {this.data.recipe.duration}</h3>
+              <p>{this.data.recipe.summary}</p>
+            </div>
 
-          //     //<Ingredients ingredients={this.data.recipe.ingredients}/>
-          //  </div>
-        // </div>
-      // );
+            <IngredientsList ingredients={this.data.recipe.ingredients} />
+          </div>
+        </div>
+      );
     }
   }
 });
 
-// Ingredients=React.createClass({
-//   renderIngredients(){
-//     return this.props.ingredients.map((ingredient) => {
-//       return <Ingredient ingredient={ingredient}/>
-//     });
-//   },
 
-//   render(){
-//     if (!this.props.ingredients.length) {
-//       return null;
-//     } else {
-//       return <div id="ingredients-list">
-//               <h3>Ingredients</h3>
-//               <ul>
-//                 {this.renderIngredients()}
-//               </ul>
-//             </div>;
-//     }
-//   }
-// });
-// Ingredient=React.createClass({
-//   render(){
-//     return null;
-//   }
-// })
+
+IngredientsList=React.createClass({
+  renderIngredients(){
+    return this.props.ingredients.map((ingredient)=>{
+      return (<Ingredient ingredient={ingredient}/>);
+    });
+  },
+
+  render(){
+    if (!this.props.ingredients.length) {
+      return null;
+    } else {
+      return (
+        <div id="ingredients-list">
+          <h3>Ingredients</h3>
+          <ul>
+            {this.renderIngredients()}
+          </ul>
+        </div>
+      );
+    }
+  }
+});
+
+
+
+Ingredient=React.createClass({
+  render(){
+    return null;
+  }
+})
