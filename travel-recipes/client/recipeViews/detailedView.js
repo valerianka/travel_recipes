5f256cb8d9f749a5c8e384adaae82eaa2f5e474a
dierat <diedra.rater@gmail.com>
2016-02-28 21:14:32 -0800

    Remove now unused Blaze code

--- travel-recipes/client/recipeViews/detailedView.js
+++ travel-recipes/client/recipeViews/detailedView.js
@@ -1,24 +1,5 @@
-Template.recipe.onRendered(()=> {
-  window.scrollTo(0, 0);
-});
-
 Template.recipe.helpers({
   isUserAndCreator() {
     return Meteor.user() ? Meteor.user().username === this.userName : false;
-  }
-});
-
-Template.ingredient.helpers({
-  currentIngredient() {
-    const ingredientID = Template.instance().data;
-    const ing = Ingredients.findOne({_id: ingredientID});
-    return ing;
-  }
-});
-
-//add button jQuery functionality in template name Ingredient
-Template.ingredient.events({
-  "click #drop-down-button"() {
-    $(Template.instance().find(".additional-details")).slideToggle();
   }
 });
