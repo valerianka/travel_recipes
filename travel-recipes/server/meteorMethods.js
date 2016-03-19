bd5e0916b61a97c0590da90bfe4d4d2d1133314c
dierat <diedra.rater@gmail.com>
2016-03-19 12:35:21 -0700

    Rewrite updateRecipes to clear out the database

--- travel-recipes/server/meteorMethods.js
+++ travel-recipes/server/meteorMethods.js
@@ -1,15 +1,12 @@
 Meteor.methods({
-  updateRecipes(){
-    Recipes.update(
-      {},
-      {$set: {ingredients: _.pluck(Ingredients.find().fetch(), "_id")}},
-      {multi: true}
-    );
-  },
-
   deleteRecipe(id){ Recipes.remove(id); },
   deleteRecipes(){ Recipes.remove({}); },
 
   deleteIngredient(id){ Ingredients.remove(id); },
-  deleteIngredients(){ Ingredients.remove({}); }
+  deleteIngredients(){ Ingredients.remove({}); },
+
+  updateRecipes(){
+    Meteor.call('deleteRecipes');
+    Meteor.call('deleteIngredients');
+  },
 });
